16b11871f61f672a996ae0d33df631ae239e37f3
Ilham <ilhamsr05@gmail.com>
2024-12-17 11:12:32 +0700

    app

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ParkingController;
 
-Route::get('/parking', function () {
+Route::get('/', function () {
     return view('parking'); // Form untuk input nomor kendaraan
 });
 
